fbcc8c276954fd7022c5fe1f9d0a454d26f4fca7
unknown <darkstardaksh@gmail.com>
2024-09-27 18:42:28 +0530

    Okay

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -1,4 +1,4 @@
-import foodModel from "../models/FoodModel.js";
+import foodModel from "../models/foodModel.js";
 import fs from 'fs' //using fs instead of cloudinary to store files on local storage
 
 // add food item
